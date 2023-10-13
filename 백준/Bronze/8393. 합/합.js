cbe9a91c41c7f2b3963df2cc3522717aa763e99e
Kim Teuk Hee <83493231+turkey-kim@users.noreply.github.com>
2023-10-13 15:21:04 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = parseInt(fs.readFileSync("/dev/stdin").toString());
+let result = 0;
+
+for (let i = 1; i <= input; i++) {
+  result += i;
+}
+
+console.log(result);
