f6db796a43cbe13fbb641a506753c11825f612cf
Kim Teuk Hee <83493231+turkey-kim@users.noreply.github.com>
2024-03-20 14:56:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 808 ms, Memory: 98308 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,36 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const [n, m, node] = input[0].split(" ").map((e) => Number(e));
+const visited = new Array(n).fill(0);
+const graph = [];
+for (let i = 0; i <= n; i++) {
+  graph.push([]);
+}
+let visitedNum = 1;
+
+function dfs(node, graph, visited) {
+  visited[node - 1] = visitedNum;
+  visitedNum++;
+
+  for (const i of graph[node]) {
+    if (!visited[i - 1]) {
+      dfs(i, graph, visited);
+    }
+  }
+}
+
+for (let i = 1; i <= m; i++) {
+  let [x, y] = input[i].split(" ").map((e) => Number(e));
+  graph[x].push(y);
+  graph[y].push(x);
+}
+
+graph.map((v) => v.sort((a, b) => b - a));
+
+dfs(node, graph, visited);
+
+console.log(visited.join("\n"));
