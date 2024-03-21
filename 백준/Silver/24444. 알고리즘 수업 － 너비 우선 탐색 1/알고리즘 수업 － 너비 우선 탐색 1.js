7bcb717f5ab664ac88a9fa1b343d01553ebcd3db
Kim Teuk Hee <83493231+turkey-kim@users.noreply.github.com>
2024-03-21 17:50:22 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 760 ms, Memory: 95620 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
@@ -1,67 +1,66 @@
-class Queue {
-  constructor() {
-    this.items = {};
-    this.headIndex = 0;
-    this.tailIndex = 0;
-  }
-  enqueue(item) {
-    this.items[this.tailIndex] = item;
-    this.tailIndex++;
-  }
-  dequeue() {
-    let item = this.items[this.headIndex];
-    delete this.items[this.headIndex];
-    this.headIndex++;
-    return item;
-  }
-  peek() {
-    return this.items[this.headIndex];
-  }
-  getLength() {
-    return this.tailIndex - this.headIndex;
-  }
-}
-
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 const input = require("fs")
-  .readFileSync("/dev/stdin")
+  .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n");
+const [N, M, R] = input.shift().split(" ").map(Number);
+let graph = [...new Array(N + 1)].map(() => []);
+let visited = new Array(N + 1).fill(0);
+let count = 0;
 
-const [n, m, r] = input[0].split(" ").map(Number);
-let graph = Array.from(new Array(n + 1), () => new Array());
-let visited = Array(n + 1).fill(0);
-let count = 1;
-
-// 그래프 생성
-for (let i = 1; i <= m; i++) {
-  let [x, y] = input[i].split(" ").map(Number);
-  graph[x].push(y);
-  graph[y].push(x);
+for (let i = 0; i < M; i++) {
+  const [X, Y] = input[i].split(" ").map(Number);
+  graph[X].push(Y);
+  graph[Y].push(X);
 }
 
-// 그래프 오름차순 정렬
-graph.forEach((x) => x.sort((a, b) => a - b));
+graph.map((arr) => arr.sort((a, b) => a - b));
 
-//bfs 로직
-function bfs(graph, start, visited) {
-  let queue = new Queue();
+class Queue {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(item) {
+    this.items[this.tail] = item;
+    this.tail++;
+  }
+  dequeue() {
+    const item = this.items[this.head];
+    delete this.items[this.head];
+    this.head++;
+    return item;
+  }
+  peek() {
+    return this.items[this.head];
+  }
+  getLength() {
+    return this.tail - this.head;
+  }
+}
+
+const bfs = (start) => {
+  const queue = new Queue();
   queue.enqueue(start);
+  count++;
+
   visited[start] = count;
 
-  while (queue.getLength() != 0) {
+  while (queue.getLength()) {
     let v = queue.dequeue();
 
-    for (let i of graph[v]) {
+    for (i of graph[v]) {
       if (!visited[i]) {
-        count++;
         queue.enqueue(i);
+        count++;
         visited[i] = count;
       }
     }
   }
-}
+};
 
-bfs(graph, r, visited);
+bfs(R);
 
 console.log(visited.slice(1).join("\n").trim());
