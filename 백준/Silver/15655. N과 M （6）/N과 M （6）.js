197992207d1667ed63fbcf92081f2edcb2af3b21
Kim Teuk Hee <83493231+turkey-kim@users.noreply.github.com>
2024-05-21 13:21:59 +0900

    [Silver III] Title: N과 M (6), Time: 292 ms, Memory: 21664 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,50 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+let visited = Array(N).fill(0);
+const node = [];
+let results = [];
+
+const nums = input
+  .shift()
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const sol = (start, depth) => {
+  visited[start] = 1;
+  node.push(nums[start]);
+
+  if (depth === M) {
+    results.push(
+      [...node]
+        .sort((a, b) => a - b)
+        .join(" ")
+        .trim()
+    );
+    visited[start] = 0;
+    node.pop();
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i] === 0) {
+      sol(i, depth + 1);
+    }
+  }
+  visited[start] = 0;
+  node.pop();
+};
+
+for (let i = 0; i < nums.length; i++) {
+  sol(i, 1);
+}
+
+const answer = new Set(results);
+console.log([...answer].join("\n").trim());
