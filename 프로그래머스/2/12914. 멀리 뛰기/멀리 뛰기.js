83e906949bdbb5a7983756aa3c33e8a7e836cf23
김특희 <83493231+turkey-kim@users.noreply.github.com>
2024-06-28 14:58:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    const MOD = 1234567;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if ( n === 2) return 2;
+
+    
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 0;  
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
+    
+    return dp[n];
+}
