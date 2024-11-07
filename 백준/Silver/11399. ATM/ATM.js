cef0dea5431cd0467fce51d869999eb9427bc3cd
김특희 <83493231+turkey-kim@users.noreply.github.com>
2024-11-07 15:53:26 +0900

    [Silver IV] Title: ATM, Time: 104 ms, Memory: 9660 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,21 +1,22 @@
-const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(filePath).toString().split("\n");
 
-const N = Number(input.shift());
-const people = input[0].split(" ").map(Number);
-let arr = [];
-let count = 0;
-let result = 0;
+const N = input[0];
 
-people.sort((a, b) => a - b);
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-people.forEach((item, i) => {
-  count += item;
-  arr.push(count);
+const waitings = [];
+
+arr.forEach((e, i) => {
+  if (i === 0) waitings.push(e);
+  else waitings.push(e + waitings[i - 1]);
 });
 
-arr.forEach((e) => {
-  result += e;
-});
-
-console.log(result);
+console.log(
+  waitings.reduce((acc, cur) => {
+    return acc + cur;
+  })
+);
