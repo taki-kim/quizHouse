47161c63d4f82b1af574c64f8e0d24a556dce380
김특희 <83493231+turkey-kim@users.noreply.github.com>
2025-02-26 16:43:31 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,29 +1,44 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = require("fs")
+
+const [N, M] = require("fs")
   .readFileSync(filePath)
   .toString()
+  .trim()
   .split(" ")
   .map(Number);
 
-const [N, M] = input;
+const arr = Array(N)
+  .fill()
+  .map((e, i) => (e = i + 1));
 
-const results = [];
-const arr = [];
+const visited = Array(N).fill(0);
 
-const DFS = () => {
-  if (arr.length === M) {
-    results.push(arr.join(" "));
+const str = [];
+const result = [];
+
+const backTracking = (preIndex) => {
+  if (str.length === M) {
+    result.push(str.join(" ").trim());
     return;
   }
 
-  for (let i = 1; i <= N; i++) {
-    if (arr.includes(i)) continue;
-    arr.push(i);
-    DFS(i);
-    arr.pop();
+  for (let i = 0; i < N; i++) {
+    if (!visited[i]) {
+      str.push(arr[i]);
+      visited[i] = 1;
+      backTracking(i);
+      str.pop();
+      visited[i] = 0;
+    }
   }
 };
 
-DFS(0);
+for (let i = 0; i < N; i++) {
+  str.push(arr[i]);
+  visited[i] = 1;
+  backTracking(i);
+  str.pop();
+  visited[i] = 0;
+}
 
-console.log(results.join("\n").trim());
+console.log(result.join("\n").trim());
