0029eb0e2cdb4c5955d9a7eeb5297cbcc1c45575
김특희 <83493231+turkey-kim@users.noreply.github.com>
2025-03-05 00:13:33 +0900

    [Silver II] Title: 촌수계산, Time: 96 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.js
+++ 백준/Silver/2644. 촌수계산/촌수계산.js
@@ -1,45 +1,41 @@
-const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n");
 
-const N = Number(input.shift());
-const [X, Y] = input.shift().split(" ").map(Number);
-const M = Number(input.shift());
+const N = +input.shift();
 
-const graph = [...new Array(N + 1)].map(() => []);
-const visited = new Array(N + 1).fill(false);
+const [x, y] = input.shift().split(" ").map(Number);
 
-let isRelated = false;
-let degree = 0;
+const M = +input.shift();
 
-const dfs = (node, depth) => {
-  visited[node] = true;
+const myGraph = new Array(N + 1).fill().map(() => []);
+const visited = new Array(N + 1).fill(-1);
 
-  if (node === Y) {
-    isRelated = true;
-    degree = depth;
-  }
+for (let i = 0; i < input.length; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  myGraph[a].push(b);
+  myGraph[b].push(a);
+}
 
-  for (i of graph[node]) {
-    if (!visited[i]) {
-      dfs(i, depth + 1);
+const bfs = (start) => {
+  const queue = [];
+  queue.push(start);
+  visited[start] = 0;
+
+  while (queue.length) {
+    const curNode = queue.shift();
+    for (i of myGraph[curNode]) {
+      if (visited[i] === -1) {
+        queue.push(i);
+        visited[i] = visited[curNode] + 1;
+      }
     }
   }
 };
 
-for (let i = 0; i < input.length; i++) {
-  const [A, B] = input[i].split(" ").map(Number);
-  graph[A].push(B);
-  graph[B].push(A);
-}
+bfs(x);
 
-dfs(X, degree);
-
-if (isRelated) {
-  console.log(degree);
-} else {
-  console.log(-1);
-}
+console.log(visited[y]);
