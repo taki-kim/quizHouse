35831b6bae889a870bde279d0e7ef39378460e2e
김특희 <83493231+taki-kim@users.noreply.github.com>
2025-09-09 17:15:06 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8140 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = [0]*10001
+
+for _ in range(n):
+    arr[int(sys.stdin.readline())] += 1
+
+for i in range(1,10001):
+    if(arr[i] == 0):
+        continue
+    else:
+        for _ in range(arr[i]):
+            print(i)
